bc95cc3648d957f3695d3335cbfc09eb0549f368
bleedingxedge <maru@bxe.me>
2016-02-02 23:32:31 -0600

    Adds procfile and prettyfies main.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 })
 
 app.use(bodyParser.json())
-app.use( bodyParser.urlencoded( { extended: true } ) )
+app.use(bodyParser.urlencoded( { extended: true } ))
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
